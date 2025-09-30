04cdcecca41e9d8188596ce87b5f70da15f1ac80
adptra01 <m.a.syaputra.94@gmail.com>
2025-09-30 20:18:03 +0700

    feat: Add model classes for Category, CategoryPost, Comment, NewsLetter, Post, PostTag, SeoDetail, Setting, ShareSnippet, Tag, and User with factories

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,21 +1,22 @@
 <?php
 
-namespace App\Models;
+namespace Firefly\FilamentBlog\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Firefly\FilamentBlog\Database\Factories\UserFactory;
+use Firefly\FilamentBlog\Traits\HasBlog;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasBlog, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
@@ -26,7 +27,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
@@ -34,15 +35,27 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function canComment()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return true;
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    protected static function newFactory()
+    {
+        return new UserFactory();
     }
 }
